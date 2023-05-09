c136b4b97d8c4ec53460925891895ab5b234c9b2
MohamedSuhailS <129146372+MohamedSuhailS@users.noreply.github.com>
2023-05-09 10:59:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -214,44 +214,44 @@ const storage = multer.diskStorage({
   }
 })
 
-app.post('/imageupload', async (req, res) => {	
-  try {
-      // 'avatar' is the name of our file input field in the HTML form
+// app.post('/imageupload', async (req, res) => {	
+//   try {
+//       // 'avatar' is the name of our file input field in the HTML form
 
-      let upload = multer({ storage: storage}).single('avatar');
+//       let upload = multer({ storage: storage}).single('avatar');
 
-      upload(req, res, function(err) {
-          // req.file contains information of uploaded file
-          // req.body contains information of text fields
+//       upload(req, res, function(err) {
+//           // req.file contains information of uploaded file
+//           // req.body contains information of text fields
 
-          if (!req.file) {
-              return res.send('Please select an image to upload');
-          }
-          else if (err instanceof multer.MulterError) {
-              return res.send(err);
-          }
-          else if (err) {
-              return res.send(err);
-          }
+//           if (!req.file) {
+//               return res.send('Please select an image to upload');
+//           }
+//           else if (err instanceof multer.MulterError) {
+//               return res.send(err);
+//           }
+//           else if (err) {
+//               return res.send(err);
+//           }
 
-          const classifiedsadd = {
-      image: req.file.filename
-    };
-    console.log(classifiedsadd)
-      res.json({ success: 1 })       
-      }); 
-  }catch (err) {console.log(err)}
-})
-const fs = require("fs");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+//           const classifiedsadd = {
+//       image: req.file.filename
+//     };
+//     console.log(classifiedsadd)
+//       res.json({ success: 1 })       
+//       }); 
+//   }catch (err) {console.log(err)}
+// })
+// const fs = require("fs");
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage: storage });
 
 app.post("/img", upload.single("photo"), (req, res) => {
   const saveImage =  userModel({
